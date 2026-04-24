0de5eff4f5bb4f992160d5879c5646c7b9152ab4
Sachithsbs <146901169+Sachithsbs@users.noreply.github.com>
2026-04-24 07:34:23 +0530

    Refactor Calculator class and methods

--- calculator_not_for_maven.java
+++ calculator_not_for_maven.java
@@ -1,52 +1,26 @@
-public class CalculatorService {
+class Calculator {
+    // 1. Data (Fields)
+    double n1, n2;
 
-    // The Calculator class (The Blueprint)
-    static class Calculator {
-        double value1;
-        double value2;
-
-        // Constructor to set the two numbers
-        Calculator(double num1, double num2) {
-            this.value1 = num1;
-            this.value2 = num2;
-        }
-
-        // Methods to perform all operations
-        double add() {
-            return value1 + value2;
-        }
-
-        double subtract() {
-            return value1 - value2;
-        }
-
-        double multiply() {
-            return value1 * value2;
-        }
-
-        double divide() {
-            if (value2 == 0) return 0; // Basic safety check
-            return value1 / value2;
-        }
-
-        double remainder() {
-            return value1 % value2;
-        }
+    // 2. Setup (Constructor)
+    Calculator(double n1, double n2) {
+        this.n1 = n1;
+        this.n2 = n2;
     }
 
-    // Main method to execute the logic
+    // 3. Actions (Methods)
+    void showResults() {
+        System.out.println("Sum: " + (n1 + n2));
+        System.out.println("Sub: " + (n1 - n2));
+        System.out.println("Mul: " + (n1 * n2));
+        System.out.println("Div: " + (n2 != 0 ? (n1 / n2) : "Error"));
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        // Create an "instance" of the calculator with two numbers
+        // Create the object and run the actions
         Calculator calc = new Calculator(20.0, 5.0);
-
-        System.out.println("Numbers: " + calc.value1 + " and " + calc.value2);
-        System.out.println("---------------------------");
-
-        // Calling the methods to get results
-        System.out.println("Sum:        " + calc.add());
-        System.out.println("Difference: " + calc.subtract());
-        System.out.println("Product:    " + calc.multiply());
-        System.out.println("Quotient:   " + calc.divide());
-        System.out.println("Remainder:  " + calc.remainder());
+        calc.showResults();
     }
 }
